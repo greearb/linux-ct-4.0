77a9af2a41f7b83b107d67b9353cefee281bec13
Ben Greear <greearb@candelatech.com>
2013-01-29 22:48:23 -0800

    sunrpc: Don't attempt to bind to AF_UNSPEC address.
    This lets calling code simply pass AF_UNSPEC addresses
    instead of adding lots of checks to conditionally pass
    NULL addresses.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -2567,9 +2567,9 @@ static struct rpc_xprt *xs_setup_xprt(struct xprt_create *args,
 	new = container_of(xprt, struct sock_xprt, xprt);
 	memcpy(&xprt->addr, args->dstaddr, args->addrlen);
 	xprt->addrlen = args->addrlen;
-	if (args->srcaddr)
+	if (args->srcaddr && (args->srcaddr->sa_family != AF_UNSPEC)) {
 		memcpy(&new->srcaddr, args->srcaddr, args->addrlen);
-	else {
+	} else {
 		int err;
 		err = xs_init_anyaddr(args->dstaddr->sa_family,
 					(struct sockaddr *)&new->srcaddr);
